e346c36717c2962c129526ed80003bd536307937
bigjoe-oti <>
2025-12-01 03:36:21 +0000

    Update dev deployment to v2.0 with /red endpoint

--- main.go
+++ main.go
@@ -12,8 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
-
 package main
 
 import (
@@ -26,12 +24,15 @@ import (
 
 func main() {
 	http.HandleFunc("/blue", blueHandler)
+	http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func blueHandler(w http.ResponseWriter, r *http.Request) {
+func redHandler(w http.ResponseWriter, r *http.Request) {
 	img := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.ZP, draw.Src)
+	draw.Draw(img, img.Bounds(), &image.Uniform{color.RGBA{255, 0, 0, 255}}, image.ZP, draw.Src)
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
 }
+
+
